978f64b2f68dee09f82c241d3fc8c2aaa5806836
[princeyadav09] <[princekumaryadav09@gmail.com]>
2023-09-25 13:04:04 +0530

    call another microservice from existing microservice

--- SpringCloud_1/src/main/java/com/api/gateway/SpringCloud/Controller/Controller.java
+++ SpringCloud_1/src/main/java/com/api/gateway/SpringCloud/Controller/Controller.java
@@ -1,13 +1,33 @@
 package com.api.gateway.SpringCloud.Controller;
 
-import org.springframework.web.bind.annotation.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
 @RestController
 @RequestMapping("/employee")
 public class Controller {
 
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public Controller(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/message")
     public String test() {
         return "Hello JavaInUse Called in First Service";
+    }
+
+    @GetMapping("/secondservicevcall")
+    public String getFromSecond(){
+        String responseEntity = restTemplate
+                .getForObject("http://localhost:8080/consumer/message", String.class );
+        return responseEntity;
     }
 }
--- SpringCloud_1/src/main/java/com/api/gateway/SpringCloud/SpringCloudApplication.java
+++ SpringCloud_1/src/main/java/com/api/gateway/SpringCloud/SpringCloudApplication.java
@@ -2,6 +2,8 @@ package com.api.gateway.SpringCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringCloudApplication {
@@ -10,4 +12,8 @@ public class SpringCloudApplication {
 		SpringApplication.run(SpringCloudApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
